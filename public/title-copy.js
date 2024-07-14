f603bdf9458be635dc707addc25cb364d2444410
ZombieGenZ <zombiegenzzz@gmail.com>
2024-07-14 19:11:47 +0700

    Fix

--- public/title-copy.js
+++
@@ -1,6 +0,0 @@
-document.title = document.title + ` | ${location.hostname}`;
-
-document.addEventListener("DOMContentLoaded", () => {
-    const date = new Date();
-    document.getElementById("Year-copy").textContent = date.getFullYear();
-});
